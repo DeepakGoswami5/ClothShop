1aff0bc08f53efbd5d604321e74a1f2d36884e69
Deepak Goswami <53327445+DeepakGoswami5@users.noreply.github.com>
2021-01-23 11:44:08 +0530

    added_sign-in-up_and_styles

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import HomePage from './pages/HomePage/HomePage';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/Shops/shops';
 import Header from '../src/components/header/header';
+import SignInUp from './pages/sign-in-up/sign-in-up';
 
 function App() {
   return (
     <div>
       <Header/>
       <Switch>
-         <Route exact path='/' component={HomePage}></Route>
-        <Route path='/shop' component={ShopPage}></Route>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route path='/shop' component={ShopPage}></Route>
+          <Route path='/sign-in-up' component={SignInUp}></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-up/sign-in-up.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './sign-in-up.scss';
+
+const SignInUp = () => (
+    <div>
+        <h1>SignInUp</h1>
+    </div>
+)
+
+export default SignInUp;
